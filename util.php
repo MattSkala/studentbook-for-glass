4292cfb6df3c2f97391243f393c82bf5472333e7
Tadeáš Ursíny <ThatIsGeek@gmail.com>
2013-08-31 17:09:29 +0200

    Login to MyStudentsBook;added URLRequest;

--- util.php
+++ util.php
@@ -22,12 +22,12 @@ require_once 'google-api-php-client/src/Google_Client.php';
 require_once 'google-api-php-client/src/contrib/Google_MirrorService.php';
 
 
-function store_credentials($user_id, $credentials) {
+function store_credentials($user_id, $credentials, $msb_token) {
   $db = init_db();
   $user_id = SQLite3::escapeString(strip_tags($user_id));
   $credentials = SQLite3::escapeString(strip_tags($credentials));
 
-  $insert = "insert or replace into credentials values ('$user_id', '$credentials')";
+  $insert = "insert or replace into credentials values ('$user_id', '$credentials', '$msb_token')";
   $db->exec($insert);
 
 }
@@ -39,7 +39,7 @@ function get_credentials($user_id) {
   $query = $db->query("select * from credentials where userid = '$user_id'");
 
   $row = $query->fetchArray(SQLITE3_ASSOC);
-  return $row['credentials'];
+  return $row;
 }
 
 function list_credentials() {
@@ -64,29 +64,8 @@ function init_db() {
 
   if ($db->querySingle($test_query) == 0) {
     $create_table = "create table credentials (userid text not null unique, " .
-        "credentials text not null);";
+        "credentials text not null, msb_token text not null);";
     $db->exec($create_table);
   }
   return $db;
-}
-
-function bootstrap_new_user() {
-  global $base_url;
-
-  $client = get_google_api_client();
-  $client->setAccessToken(get_credentials($_SESSION['userid']));
-
-  // A glass service for interacting with the Mirror API
-  $mirror_service = new Google_MirrorService($client);
-
-  $timeline_item = new Google_TimelineItem();
-  $timeline_item->setText("Welcome to the Mirror API PHP Quick Start");
-
-  insert_timeline_item($mirror_service, $timeline_item, null, null);
-
-  insert_contact($mirror_service, "php-quick-start", "PHP Quick Start",
-      $base_url . "/static/images/chipotle-tube-640x360.jpg");
-
-  subscribe_to_notifications($mirror_service, "timeline",
-    $_SESSION['userid'], $base_url . "/notify.php");
 }
