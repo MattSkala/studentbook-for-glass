a0254b349b14d55b4cf7708e0309bef202897d5e
Matouš Skála <skala.matous@gmail.com>
2013-08-31 14:40:29 +0200

    Fixed url

--- config.php
+++ config.php
@@ -22,7 +22,7 @@ $api_client_id = "API_CLIENT_ID";
 $api_client_secret = "API_CLIENT_SECRET";
 $api_simple_key = "API_KEY";
 
-$base_url = "http://localhost/studentbook-for-android";
+$base_url = "http://localhost/studentbook-for-glass";
 
 // This should be writable by your web server's user
 $sqlite_database = "/tmp/database.sqlite";
